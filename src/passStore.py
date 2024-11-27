ca85c10e679f51ccde0e7e337a94aed366da10bc
damnoctopus <adithyasr@duck.com>
2024-11-27 19:33:40 +0530

    update

--- src/passStore.py
+++ src/passStore.py
@@ -3,12 +3,12 @@ import os
 import func as fc
 
 def main():
-
+        factorauth = True
         if not os.path.exists('fileData/secret.key'):
             fs.generate_key()
 
         while True:
-            choice = int(input("Would you like to\n 1.Add\n 2.Retrieve\n 3.Quit \n"))
+            choice = int(input("Would you like to\n 1.Add\n 2.Retrieve\n 3.Setup 2FA \n 4.Quit \n"))
 
             if choice == 1:
                 # code add a new password
@@ -30,25 +30,31 @@ def main():
 
             elif choice == 2:
                 # retrieve a password
-                website = input("Enter the website to retrieve: ")
-                passwords = fs.load_passwords()
-                found = False
+                if factorauth is True:
+                    website = input("Enter the website to retrieve: ")
+                    passwords = fs.load_passwords()
+                    found = False
 
-                for entry in passwords:
-                    if entry[0] == website:
-                        decrypted_password = fs.decrypt_data(entry[2].encode())
-                        print(f"Username: {entry[1]}, Password: {decrypted_password}")
-                        found = True
-                        break
+                    for entry in passwords:
+                        if entry[0] == website:
+                            decrypted_password = fs.decrypt_data(entry[2].encode())
+                            print(f"Username: {entry[1]}, Password: {decrypted_password}")
+                            found = True
+                            break
 
-                if not found:
-                    print("No password found for that website.")
+                    if not found:
+                        print("No password found for that website.")
+
+                else:
+                    print("2Factor Authentication not passed ")
 
             elif choice == 3:
+                print("hehe")
+            elif choice == 4:
                 break
 
             else:
-                print("Invalid option. Please choose 1,2,or 3")
+                print("Invalid option. Please choose 1,2,3 or 4")
 
 
 main()
