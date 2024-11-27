64fd2243f859573bfb47c50a823249cfc5b6d0ed
calvyin <cynicmuffin@gmail.com>
2024-11-27 20:34:45 +0530

    wad

--- src/passStore.py
+++ src/passStore.py
@@ -5,7 +5,6 @@ import func as fc
 
 
 def main():
-        factorauth = True
         if not os.path.exists('fileData/secret.key'):
             fs.generate_key()
         factor_setup = False
@@ -34,29 +33,32 @@ def main():
 
             elif choice == 2:
                 # retrieve a password
-                if factorauth is True:
+                if factor_setup is True:
                     website = input("Enter the website to retrieve: ")
-                    passwords = fs.load_passwords()
-                    found = False
+                    factor_verify = tfa.two_factor_auth()
+                    if factor_verify:
+                        passwords = fs.load_passwords()
+                        found = False
 
-                    for entry in passwords:
-                        if entry[0] == website:
-                            decrypted_password = fs.decrypt_data(entry[2].encode())
-                            print(f"Username: {entry[1]}, Password: {decrypted_password}")
-                            found = True
-                            break
+                        for entry in passwords:
+                            if entry[0] == website:
+                                decrypted_password = fs.decrypt_data(entry[2].encode())
+                                print(f"Username: {entry[1]}, Password: {decrypted_password}")
+                                found = True
+                                break
 
-                    if not found:
-                        print("No password found for that website.")
+                        if not found:
+                            print("No password found for that website.")
+                    else:
+                        print("Failed to verify user")
 
                 else:
-                    print("2Factor Authentication not passed ")
+                    print("Two Factor Authentication not setup")
 
             elif choice == 3:
                 if factor_setup is False:
                     factor_setup = tfa.two_factor_auth()
                     print("Two factor authentication has been setup")
-                    break
                 else:
                     print("Two factor authentication has already been setup if you wish to reset your 2FA please choose"
                           " option 4")
@@ -69,7 +71,7 @@ def main():
                 break
 
             else:
-                print("Invalid option. Please choose 1,2,3 or 4")
+                print("Invalid option. Please choose a valid option.")
 
 
 main()
